0f60c0fc9b5c24d38fdedb1f4eed91e65113bc69
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-09-24 14:41:54 -0400

    database name - whole team

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/'
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/sportikusData'
 
 mongoose
     .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
