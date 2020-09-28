82c1c3e8c8a7a0ac6a5c0bbc9d58c88785cbb885
Ash Gonzales <ashgonzales@Ashs-MacBook-Pro.local>
2020-09-28 10:19:36 -0600

    Whole Team - Create About us and Carousel (not in order)

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import About from '../../components/About/About';
+import CarouselComp from '../../components/CarouselComp/CarouselComp';
 import "./Home.css"
 
 const Home = () => {
   return (
     <div>
       <h1>Sportikus</h1>
+      <About />
+      <CarouselComp />
     </div>
   );
 };
