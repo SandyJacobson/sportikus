a4785cab82588f0a240c853e5eab281e3b76f6d7
enwumeh <54593478+enwumeh@users.noreply.github.com>
2020-09-29 14:43:30 -0500

    footer added-EN

--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer'
 
 const Layout = (props) => {
   return (
     <div>
       <Header />
       {props.children}
+      <Footer />
     </div>
   );
 };
