abca0d2cd05e3af3aa21c71914ce9e8a62e4d3a9
Ash Gonzales <ashgonzales@Ashs-MacBook-Pro.local>
2020-10-01 14:48:20 -0600

    AG Edited hr line

--- models/product.js
+++ models/product.js
@@ -1,17 +1,27 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const Product = new Schema(
   {
-      name: { type: String, required: true },
-      price: { type: String, required: true },
-      imgURLOne: { type: String, required: true },
-      imgURLTwo: { type: String, required: true },
-      imgURLThree: { type: String, required: true },
-      description: { type: String, required: true },
-      detail: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: String, required: true },
+    imgURLOne: { type: String, required: true },
+    imgURLTwo: { type: String, required: true },
+    imgURLThree: { type: String, required: true },
+    description: { type: String, required: true },
+    detail: { type: String, required: true },
+    reviews: [
+      {
+        title: { type: String, required: true },
+        rating: { type: Number, required: true },
+        content: { type: String, required: true },
+        date: { type: String, required: true },
+        author: { type: String, required: true },
+        location: { type: String, required: true },
+      }
+    ],
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('products', Product)
+module.exports = mongoose.model("products", Product);
